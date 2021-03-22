eb754b8190e63ad853bbb495578370b4e5f3e7cd
DanielOlivaw <77257095+DanielOlivaw@users.noreply.github.com>
2021-03-22 16:01:59 -0700

    Cleaning up engine/pokemon/categories.asm

--- engine/pokemon/categories.asm
+++ engine/pokemon/categories.asm
@@ -7,48 +7,6 @@ GetMoveCategoryName:
 	ld a, MOVE_TYPE
 	call GetMoveAttribute
 
-	; dec a
-	; ld bc, MOVE_LENGTH
-	; ld hl, Moves + MOVE_TYPE
-	; call AddNTimes
-	; ld a, BANK(Moves)
-
-	; dec a
-	; ld bc, MOVE_LENGTH
-	; ld hl, Moves
-	; call AddNTimes
-	; ld de, wStringBuffer1
-	; ld a, BANK(Moves)
-	; call FarCopyBytes
-
-	; ld de, wStringBuffer1
-	; ld [de], a
-	; inc de
-	; push bc
-	; call GetMoveIndexFromID
-	; ld b, h
-	; ld c, l
-	; ld hl, Moves
-	; ld a, BANK(Moves)
-	; call LoadIndirectPointer
-	; pop bc
-	; ld bc, MOVE_LENGTH - 1
-	; jp FarCopyBytes
-
-	; ld [de], a
-	; inc de
-	; push bc
-	; call GetMoveIndexFromID
-	; ld b, h
-	; ld c, l
-	; ld hl, Moves + MOVE_TYPE
-	; ld a, BANK(Moves)
-	; call LoadIndirectPointer
-	; pop bc
-	; ld bc, MOVE_LENGTH - 1
-
-	; call GetFarByte
-
 ; Mask out the type
 	and $ff ^ TYPE_MASK
 ; Shift the category bits into the range 0-2
