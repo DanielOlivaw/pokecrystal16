96869ff11fa000a1f2d9ccd35a00b8ecad449b98
U-DJB-LT2\Daniel <djb@baldwinhome.info>
2021-11-07 10:34:35 -0800

    Egg Moves: Gen 5

--- data/pokemon/egg_moves_5.asm
+++ data/pokemon/egg_moves_5.asm
@@ -55,7 +55,7 @@ EggMovePointers5::
 	dw NoEggMoves5
 	dw FerroseedEggMoves
 	dw NoEggMoves5
-	dw TynamoEggMoves
+	dw NoEggMoves5
 	dw NoEggMoves5
 	dw NoEggMoves5
 	dw ElgyemEggMoves
@@ -72,7 +72,7 @@ EggMovePointers5::
 	dw MienfooEggMoves
 	dw NoEggMoves5
 	dw DruddigonEggMoves
-	dw GolettEggMoves
+	dw NoEggMoves5
 	dw NoEggMoves5
 	dw PawniardEggMoves
 	dw NoEggMoves5
@@ -85,49 +85,181 @@ EggMovePointers5::
 	dw NoEggMoves5
 
 OshawottEggMoves:
+	dw AIR_SLASH
+	dw BRINE
+	dw DETECT
+	dw NIGHT_SLASH
+	dw SACRED_SWORD
+	dw SCREECH
+	dw TRUMP_CARD
 	dw -1 ; end
 
 LillipupEggMoves:
+	dw COVET
+	dw HOWL
+	dw LICK
+	dw MUD_SLAP
+	dw YAWN
+	dw CHARM
+	dw FIRE_FANG
+	dw ICE_FANG
+	dw PSYCHIC_FANGS
+	dw PURSUIT
+	dw SAND_ATTACK
+	dw THUNDER_FANG
 	dw -1 ; end
 
 PurrloinEggMoves:
+	dw COVET
+	dw DOUBLE_TEAM
+	dw QUICK_ATTACK
+	dw SLASH
+	dw YAWN
+	dw CHARM
+	dw ENCORE
+	dw FAKE_TEARS
+	dw FAINT_ATTACK
+	dw PAY_DAY
 	dw -1 ; end
 
 MunnaEggMoves:
+	dw HEALING_WISH
+	dw BARRIER
+	dw BATON_PASS
+	dw SONICBOOM
+	dw SWIFT
 	dw -1 ; end
 
 RoggenrolaEggMoves:
+	dw AUTOTOMIZE
+	dw TAKE_DOWN
+	dw HEAVY_SLAM
+	dw LOCK_ON
+	dw MAGNITUDE
 	dw -1 ; end
 
 WoobatEggMoves:
+	dw FLATTER
+	dw KNOCK_OFF
+	dw PSYCHO_SHIFT
+	dw ROOST
+	dw SUPERSONIC
+	dw CAPTIVATE
+	dw CHARM
+	dw FAKE_TEARS
+	dw STORED_POWER
+	dw SYNCHRONOISE
+	dw VENOM_DRENCH
 	dw -1 ; end
 
 AudinoEggMoves:
+	dw HEALING_WISH
+	dw SWEET_KISS
+	dw WISH
+	dw YAWN
+	dw AMNESIA
+	dw DRAINING_KISS
+	dw ENCORE
+	dw HEAL_BELL
+	dw SERENE_BELL
+	dw LUCKY_CHANT
 	dw -1 ; end
 
 TimburrEggMoves:
+	dw COMET_PUNCH
+	dw COUNTER
+	dw DETECT
+	dw DRAIN_PUNCH
+	dw FORCE_PALM
+	dw FORESIGHT
+	dw MACH_PUNCH
+	dw POWER_UP_PUNCH
+	dw REVERSAL
+	dw SMELLINGSALT
 	dw -1 ; end
 
 SewaddleEggMoves:
+	dw AGILITY
+	dw AIR_SLASH
+	dw BATON_PASS
+	dw ME_FIRST
+	dw MIND_READER
+	dw RAZOR_WIND
+	dw SCREECH
+	dw SILVER_WIND
 	dw -1 ; end
 
 VenipedeEggMoves:
+	dw PIN_MISSILE
+	dw SPIKES
+	dw TAKE_DOWN
+	dw TOXIC_SPIKES
+	dw TWINEEDLE
+	dw BITE
+	dw FURY_CUTTER
 	dw -1 ; end
 
 CottoneeEggMoves:
+	dw BEAT_UP
+	dw CAPTIVATE
+	dw ENCORE
+	dw FAKE_TEARS
+	dw GRASSWHISTLE
+	dw MEMENTO
+	dw MISTY_TERRAIN
+	dw SWITCHEROO
+	dw TICKLE
 	dw WIND_RIDE
 	dw -1 ; end
 
 PetililEggMoves:
+	dw BIDE
+	dw CHARM
+	dw ENCORE
+	dw GRASSWHISTLE
+	dw HEALING_WISH
+	dw INGRAIN
+	dw SWEET_SCENT
 	dw -1 ; end
 
 BasculinEggMoves:
+	dw AGILITY
+	dw BRINE
+	dw BUBBLEBEAM
+	dw HEAD_SMASH
+	dw MUDDY_WATER
+	dw RAGE
+	dw REVENGE
+	dw SWIFT
+	dw ENDEAVOR
 	dw -1 ; end
 
 SandileEggMoves:
+	dw BEAT_UP
+	dw COUNTER
+	dw DOUBLE_EDGE
+	dw FIRE_FANG
+	dw FOCUS_ENERGY
+	dw ME_FIRST
+	dw MEAN_LOOK
+	dw POWER_TRIP
+	dw PURSUIT
+	dw THUNDER_FANG
+	dw AQUA_TAIL
+	dw SPITE
 	dw -1 ; end
 
 DarumakaGEggMoves:
+	dw FLAME_WHEEL
+	dw FOCUS_PUNCH
+	dw FREEZE_DRY
+	dw HAMMER_ARM
+	dw INCINERATE
+	dw POWER_UP_PUNCH
+	dw TAKE_DOWN
+	dw YAWN
+	dw ENCORE
+	dw FOCUS_ENERGY
 	dw -1 ; end
 
 MaractusEggMoves:
@@ -136,76 +268,230 @@ MaractusEggMoves:
 	dw BOUNCE
 	dw BULLET_SEED
 	dw GRASSWHISTLE
+	dw LEECH_SEED
 	dw SEED_BOMB
 	dw SPIKES
 	dw -1 ; end
 
 DwebbleEggMoves:
+	dw BLOCK
+	dw COUNTER
+	dw IRON_DEFENSE
+	dw NIGHT_SLASH
+	dw SAND_TOMB
+	dw SPIKES
+	dw KNOCK_OFF
 	dw -1 ; end
 
 ScraggyEggMoves:
+	dw ACID_SPRAY
+	dw AMNESIA
+	dw COUNTER
+	dw DETECT
+	dw DRAGON_DANCE
+	dw DRAIN_PUNCH
+	dw FAKE_OUT
+	dw FAINT_ATTACK
+	dw POWER_UP_PUNCH
 	dw -1 ; end
 
 YamaskGEggMoves:
+	dw MEMENTO
+	dw CRAFTY_SHIELD
+	dw DISABLE
+	dw FAKE_TEARS
+	dw NASTY_PLOT
+	dw NIGHTMARE
+	dw TOXIC_SPIKES
 	dw -1 ; end
 
 TirtougaEggMoves:
+	dw BIDE
+	dw BODY_SLAM
+	dw FLAIL
+	dw GUARD_SWAP
+	dw IRON_DEFENSE
+	dw KNOCK_OFF
+	dw LIQUIDATION
+	dw ROCK_THROW
+	dw SLAM
+	dw BLOCK
+	dw ROLLOUT
 	dw -1 ; end
 
 ArchenEggMoves:
+	dw BITE
+	dw HEAD_SMASH
+	dw KNOCK_OFF
+	dw SWITCHEROO
+	dw DOUBLE_TEAM
 	dw -1 ; end
 
 SolosisEggMoves:
+	dw ACID_ARMOR
+	dw ASTONISH
+	dw CONFUSE_RAY
+	dw NIGHT_SHADE
+	dw TRICK
 	dw -1 ; end
 
 FrillishEggMoves:
+	dw ACID_ARMOR
+	dw CONFUSE_RAY
+	dw CONSTRICT
+	dw MIST
+	dw PAIN_SPLIT
+	dw RECOVER
+	dw BUBBLEBEAM
+	dw STRENGTH_SAP
 	dw -1 ; end
 
 JoltikEggMoves:
+	dw CROSS_POISON
+	dw DISABLE
+	dw FAINT_ATTACK
+	dw LUNGE
+	dw PIN_MISSILE
+	dw POISON_STING
+	dw PURSUIT
+	dw DOUBLE_TEAM
+	dw STRUGGLE_BUG
 	dw -1 ; end
 
 FerroseedEggMoves:
-	dw -1 ; end
-
-TynamoEggMoves:
+	dw ACID_SPRAY
+	dw LEECH_SEED
+	dw SEED_BOMB
+	dw SPIKES
+	dw KNOCK_OFF
 	dw -1 ; end
 
 ElgyemEggMoves:
+	dw ASTONISH
+	dw BARRIER
+	dw COSMIC_POWER
+	dw DISABLE
+	dw GUARD_SWAP
+	dw NASTY_PLOT
+	dw POWER_SWAP
+	dw TELEPORT
+	dw DESTINY_BOND
 	dw -1 ; end
 
 LitwickEggMoves:
+	dw ACID
+	dw ACID_ARMOR
+	dw CAPTIVATE
+	dw CLEAR_SMOG
+	dw HAZE
+	dw HEAT_WAVE
+	dw POWER_SPLIT
 	dw -1 ; end
 
 AxewEggMoves:
+	dw COUNTER
+	dw ENDEAVOR
+	dw FOCUS_ENERGY
+	dw HARDEN
+	dw NIGHT_SLASH
+	dw RAZOR_WIND
+	dw REVERSAL
+	dw FIRST_IMPRESSION
 	dw -1 ; end
 
 CubchooEggMoves:
+	dw AVALANCHE
+	dw ENCORE
+	dw FOCUS_PUNCH
+	dw NIGHT_SLASH
+	dw YAWN
 	dw -1 ; end
 
 StunfiskEggMoves:
+	dw ASTONISH
+	dw EERIE_IMPULSE
+	dw ME_FIRST
+	dw PAIN_SPLIT
+	dw REFLECT_TYPE
+	dw SHOCK_WAVE
+	dw SPARK
+	dw SPITE
+	dw YAWN
 	dw -1 ; end
 
 MienfooEggMoves:
+	dw BATON_PASS
+	dw FEINT
+	dw KNOCK_OFF
+	dw LOW_KICK
+	dw ME_FIRST
+	dw SMELLINGSALT
+	dw VITAL_THROW
+	dw FOCUS_PUNCH
 	dw -1 ; end
 
 DruddigonEggMoves:
-	dw -1 ; end
-
-GolettEggMoves:
+	dw CRUSH_CLAW
+	dw FAINT_ATTACK
+	dw FIRE_FANG
+	dw GLARE
+	dw METAL_CLAW
+	dw POISON_TAIL
+	dw PURSUIT
+	dw SUCKER_PUNCH
+	dw THUNDER_FANG
+	dw NIGHT_SLASH
 	dw -1 ; end
 
 PawniardEggMoves:
+	dw MEAN_LOOK
+	dw PSYCHO_CUT
+	dw PURSUIT
+	dw REVENGE
+	dw SUCKER_PUNCH
 	dw -1 ; end
 
 HeatmorEggMoves:
+	dw BELCH
+	dw BODY_SLAM
+	dw FAINT_ATTACK
+	dw HEAT_WAVE
+	dw NIGHT_SLASH
+	dw PURSUIT
+	dw SUCKER_PUNCH
+	dw TICKLE
+	dw WRAP
 	dw -1 ; end
 
 DurantEggMoves:
+	dw BATON_PASS
+	dw FAINT_ATTACK
+	dw SCREECH
+	dw THUNDER_FANG
+	dw FIRST_IMPRESSION
+	dw FLAIL
+	dw METAL_BURST
+	dw STRUGGLE_BUG
 	dw -1 ; end
 
 DeinoEggMoves:
+	dw ASTONISH
+	dw BELCH
+	dw DOUBLE_HIT
+	dw FIRE_FANG
+	dw HEAD_SMASH
+	dw ICE_FANG
+	dw SCREECH
+	dw THUNDER_FANG
 	dw -1 ; end
 
 LarvestaEggMoves:
+	dw FORESIGHT
+	dw HARDEN
+	dw MAGNET_RISE
+	dw MORNING_SUN
+	dw STRING_SHOT
+	dw ABSORB
+	dw THRASH
 NoEggMoves5:
 	dw -1 ; end
