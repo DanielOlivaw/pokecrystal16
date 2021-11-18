26c5e491fd84778aab77a39333e84c6cec92d526
U-DJB-LT2\Daniel <djb@baldwinhome.info>
2021-11-17 23:55:18 -0800

    New fleeing Pokemon lists

--- data/wild/flee_mons.asm
+++ data/wild/flee_mons.asm
@@ -2,33 +2,28 @@ FleeMons:
 ; referenced by TryEnemyFlee and FastBallMultiplier
 
 SometimesFleeMons:
-	dw MAGNEMITE
-	dw GRIMER
-	dw TANGELA
+	dw MIME_JR
 	dw MR__MIME_G
-	dw EEVEE
-	dw PORYGON
+	dw CUBONE
 	dw DRATINI
-	dw DRAGONAIR
-	dw TOGETIC
-	dw UMBREON
 	dw UNOWN
 	dw SNUBBULL
+	dw PHANPY
+	dw TEDDIURSA
 	dw HERACROSS
+	dw POOCHYENA
+	dw DRIFBLIM
+	dw LILLIPUP
+	dw DEERLING
+	dw NICKIT
 	dw -1
 
 OftenFleeMons:
-	dw CUBONE
-	dw ARTICUNO_G
-	dw ZAPDOS_G
-	dw MOLTRES_G
-	dw QUAGSIRE
-	dw CUBCHOO
-	dw PHANPY
-	dw TEDDIURSA
+	dw DELIBIRD
+	dw RAIKOU
+	dw ENTEI
 	dw -1
 
 AlwaysFleeMons:
-	dw RAIKOU
-	dw ENTEI
+	dw WIMPOD
 	dw -1
