cdfc223318deaa6527546e5639f805bf6475bc76
U-DESKTOP-1V3M5EK\Daniel <djb@baldwinhome.info>
2022-05-14 16:16:22 -0700

    Make the lottery daily instead of weekly

--- engine/overworld/time.asm
+++ engine/overworld/time.asm
@@ -96,6 +96,7 @@ CheckDailyResetTimer::
 	ld [hli], a ; wDailyFlags2
 	ld [hli], a ; wSwarmFlags
 	ld [hl], a  ; wSwarmFlags + 1
+	ld [wLuckyNumberShowFlag], a
 	ld hl, wDailyRematchFlags
 rept 4
 	ld [hli], a
