0a38e98c360599cca417e7f2d241fd7574d571ac
U-DESKTOP-1V3M5EK\Daniel <djb@baldwinhome.info>
2022-05-14 18:03:42 -0700

    Automatically reuse Repel (pokecrystal tutorial)

--- engine/events/misc_scripts_2.asm
+++ engine/events/misc_scripts_2.asm
@@ -10,6 +10,22 @@ RepelWoreOffScript::
 	text_far _RepelWoreOffText
 	text_end
 
+UseAnotherRepelScript::
+	opentext
+	readmem wRepelType
+	getitemname STRING_BUFFER_3, USE_SCRIPT_VAR
+	writetext .text
+	yesorno
+	iffalse .done
+	callasm DoItemEffect
+.done
+	closetext
+	end
+
+.text:
+	text_far _UseAnotherRepelText
+	text_end
+
 HiddenItemScript::
 	opentext
 	readmem wHiddenItemID
