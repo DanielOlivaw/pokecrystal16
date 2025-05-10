bf8dd3cb5a46a4a436781451964d6086070fe46a
DanielOlivaw <djb@baldwinhome.info>
2025-05-10 16:36:50 -0700

    Fix Mamoswine front sprite

--- data/pokemon/base_stats/mamoswine.asm
+++ data/pokemon/base_stats/mamoswine.asm
@@ -10,7 +10,7 @@
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
 	db 20 ; step cycles to hatch
-	INCBIN "gfx/pokemon/piloswine/front.dimensions"
+	INCBIN "gfx/pokemon/mamoswine/front.dimensions"
 	db 0, 0, 0, 0 ; padding
 	db GROWTH_SLOW ; growth rate
 	dn EGG_GROUND, EGG_GROUND ; egg groups
